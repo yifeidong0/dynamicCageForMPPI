0c16369aa6118d4cf106e7b05b0a1b9793480e16
YvesDong <602522935@qq.com>
2024-01-18 08:56:07 +0100

    eval

--- scripts/eval_postprocess.py
+++ scripts/eval_postprocess.py
@@ -5,13 +5,13 @@ import csv
 import os
 import numpy as np
 
-plannername = 'ao-rrt' # 'ao-est', 'rrt*', 'ao-rrt'
-prname = 'PlanePush' # 'BalanceGrasp', 'PlanePush', 'PlanePushRrtstar', 'BoxPivot', 'WaterSwing', 'Shuffling'
+# plannername = 'ao-rrt' # 'ao-est', 'rrt*', 'ao-rrt'
+prname = 'BalanceGrasp' # 'BalanceGrasp', 'PlanePush', 'PlanePushRrtstar', 'BoxPivot', 'WaterSwing', 'Shuffling'
 num_via_points = 10
-num_trajs = 50
+num_trajs = 30
 
-if prname == 'PlanePush' or prname == 'PlanePushRrtstar':
-    f_states = 'data/evaluation/push_fixture/rand_traj/dataset/scripted_movement_viapoints_PlanePush.csv'
+if prname == 'PlanePush':
+    # f_states = 'data/evaluation/push_fixture/rand_traj/dataset/scripted_movement_viapoints_PlanePush.csv'
     f_success_labels = 'data/evaluation/push_fixture/rand_traj/dataset/scripted_movement_success_labels_PlanePush.csv'
     f_maneuver_labels = 'data/evaluation/push_fixture/rand_traj/dataset/scripted_movement_maneuver_labels_PlanePush.csv'
     f_aorrt_metrics = 'data/evaluation/push_fixture/rand_traj/approaches/ao-rrt-metrics/ao_rrt.csv'
@@ -19,29 +19,37 @@ if prname == 'PlanePush' or prname == 'PlanePushRrtstar':
     f_heuristics = 'data/evaluation/push_fixture/rand_traj/approaches/heuristics/scripted_movement_heuristics_PlanePush.csv'
     f_soft_fixture = 'data/evaluation/push_fixture/rand_traj/approaches/rrtstar-softfixture/rrtstar.csv'
     f_quasistatic = 'data/evaluation/push_fixture/rand_traj/approaches/ao-rrt-quasistatic/ao_rrt.csv'
-# if prname == 'BalanceGrasp':
-#     filenames = ['data/evaluation/balance_grasp/test_data/scripted_movement_viapoints_BalanceGrasp_fail.csv',
-#                  'data/evaluation/balance_grasp/test_data/scripted_movement_viapoints_BalanceGrasp_success.csv']
+    m_thres = 0.4
+    s_thres = 0.5
+    dis_thres = -1e-1
+    force_thres = 1.0
+if prname == 'BalanceGrasp':
+    f_success_labels = 'data/evaluation/balance_grasp/rand_traj/dataset/scripted_movement_success_labels_BalanceGrasp.csv'
+    f_maneuver_labels = 'data/evaluation/balance_grasp/rand_traj/dataset/scripted_movement_maneuver_labels_BalanceGrasp.csv'
+    f_aorrt_metrics = 'data/evaluation/balance_grasp/rand_traj/approaches/ao-rrt-metrics/ao_rrt.csv'
+    f_aoest_metrics = ''
+    f_soft_fixture = ''
+    f_quasistatic = 'data/evaluation/balance_grasp/rand_traj/approaches/ao-rrt-quasistatic/ao_rrt.csv'
+    f_heuristics = 'data/evaluation/balance_grasp/rand_traj/approaches/heuristics/scripted_movement_heuristics_BalanceGrasp.csv'
+    m_thres = 0.1
+    s_thres = 0.5
+    dis_thres = -1e-2
+    force_thres = 1.0
+
 # if prname == 'BoxPivot':
-#     filenames = [
-#                 'data/boxpivot/scripted_movement_viapoints_BoxPivot_k2.0_friction1.0.csv',
-#                  ]
 # if prname == 'Shuffling':
-#     filenames = [
-#                 'data/shuffling/scripted_movement_viapoints_Shuffling.csv',
-#                  ]
-    
-# Read from the CSV file
-rows = [] # 500
-id_trajs = [] # 500
-id_data = [] # 500
-with open(f_states, 'r') as file:
-    csv_reader = csv.reader(file)
-    header = next(csv_reader)
-    for id, row in enumerate(csv_reader):
-        id_trajs.append(int(row[0]))
-        id_data.append(int(row[1]))
-        rows.append([float(d) for d in row[2:]])
+
+# # Read from the CSV file
+# rows = [] # 500
+# id_trajs = [] # 500
+# id_data = [] # 500
+# with open(f_states, 'r') as file:
+#     csv_reader = csv.reader(file)
+#     header = next(csv_reader)
+#     for id, row in enumerate(csv_reader):
+#         id_trajs.append(int(row[0]))
+#         id_data.append(int(row[1]))
+#         rows.append([float(d) for d in row[2:]])
 
 success_labels = [] # 50
 with open(f_success_labels, 'r') as file:
@@ -66,22 +74,22 @@ with open(f_aorrt_metrics, 'r') as file:
         success_metrics_aorrt.append(float(row[5]))
         maneuverability_metric_aorrt.append(float(row[6]))
 
-success_metrics_aoest = []
-maneuverability_metric_aoest = []
-with open(f_aoest_metrics, 'r') as file:
-    csv_reader = csv.reader(file)
-    header = next(csv_reader)
-    for id, row in enumerate(csv_reader):
-        success_metrics_aoest.append(float(row[5]))
-        maneuverability_metric_aoest.append(float(row[6]))
+# success_metrics_aoest = []
+# maneuverability_metric_aoest = []
+# with open(f_aoest_metrics, 'r') as file:
+#     csv_reader = csv.reader(file)
+#     header = next(csv_reader)
+#     for id, row in enumerate(csv_reader):
+#         success_metrics_aoest.append(float(row[5]))
+#         maneuverability_metric_aoest.append(float(row[6]))
 
-soft_fixture_metrics = []
-with open(f_soft_fixture, 'r') as file:
-    csv_reader = csv.reader(file)
-    header = next(csv_reader)
-    for id, row in enumerate(csv_reader):
-        d = 100.0 if row[4] == 'inf' else float(row[4])
-        soft_fixture_metrics.append(d)
+# soft_fixture_metrics = []
+# with open(f_soft_fixture, 'r') as file:
+#     csv_reader = csv.reader(file)
+#     header = next(csv_reader)
+#     for id, row in enumerate(csv_reader):
+#         d = 100.0 if row[4] == 'inf' else float(row[4])
+#         soft_fixture_metrics.append(d)
 
 success_metrics_quasistatic = []
 maneuverability_metrics_quasistatic = []
@@ -174,50 +182,50 @@ def get_s_metric(minval, maxval, thres, metric_original):
 
 ##################################
 print('######1. AO-RRT Maneuverability Metric######')
-minval, maxval, thres, metric_original = 0.0, 1.0, 0.4, maneuverability_metric_aorrt
+minval, maxval, thres, metric_original = 0.0, 1.0, m_thres, maneuverability_metric_aorrt
 get_m_metric(minval, maxval, thres, metric_original)
 
 print('######AO-RRT Success Metric######')
-minval, maxval, thres, metric_original = 0.0, 1.0, 0.5, success_metrics_aorrt
+minval, maxval, thres, metric_original = 0.0, 1.0, s_thres, success_metrics_aorrt
 get_s_metric(minval, maxval, thres, metric_original)
 
-print('######2. AO-EST Maneuverability Metric######')
-minval, maxval, thres, metric_original = 0.0, 1.0, 0.4, maneuverability_metric_aoest
-get_m_metric(minval, maxval, thres, metric_original)
+# print('######2. AO-EST Maneuverability Metric######')
+# minval, maxval, thres, metric_original = 0.0, 1.0, m_thres, maneuverability_metric_aoest
+# get_m_metric(minval, maxval, thres, metric_original)
 
-print('######AO-EST Success Metric######')
-minval, maxval, thres, metric_original = 0.0, 1.0, 0.5, success_metrics_aoest
-get_s_metric(minval, maxval, thres, metric_original)
+# print('######AO-EST Success Metric######')
+# minval, maxval, thres, metric_original = 0.0, 1.0, s_thres, success_metrics_aoest
+# get_s_metric(minval, maxval, thres, metric_original)
 
-print('######3. RRT* Soft Fixture Maneuverability Metric######')
-minval, maxval, thres, metric_original = 0.09, 3.01, 0.4, soft_fixture_metrics
-get_m_metric(minval, maxval, thres, metric_original)
+# print('######3. RRT* Soft Fixture Maneuverability Metric######')
+# minval, maxval, thres, metric_original = 0.09, 3.01, m_thres, soft_fixture_metrics
+# get_m_metric(minval, maxval, thres, metric_original)
 
-print('######RRT* Soft Fixture Success Metric######')
-minval, maxval, thres, metric_original = 0.09, 3.01, 0.5, soft_fixture_metrics
-get_s_metric(minval, maxval, thres, metric_original)
+# print('######RRT* Soft Fixture Success Metric######')
+# minval, maxval, thres, metric_original = 0.09, 3.01, s_thres, soft_fixture_metrics
+# get_s_metric(minval, maxval, thres, metric_original)
 
 print('######4. AO-RRT Quasistatic Motion Maneuverability Metric######')
-minval, maxval, thres, metric_original = 0., 1.0, 0.4, maneuverability_metrics_quasistatic
+minval, maxval, thres, metric_original = 0., 1.0, m_thres, maneuverability_metrics_quasistatic
 get_m_metric(minval, maxval, thres, metric_original)
 
 print('######AO-RRT Quasistatic Motion Success Metric######')
-minval, maxval, thres, metric_original = 0., 1.0, 0.5, success_metrics_quasistatic
+minval, maxval, thres, metric_original = 0., 1.0, s_thres, success_metrics_quasistatic
 get_s_metric(minval, maxval, thres, metric_original)
 
 # 5,6 are good metrics but hard to determine the threshold
 print('######5. Heuristic - Closest Distance - Maneuverability Metric######')
-minval, maxval, thres, metric_original = 0., -1.0, -1e-1, closeset_distance_metrics
+minval, maxval, thres, metric_original = 0., -1.0, dis_thres, closeset_distance_metrics
 get_m_metric(minval, maxval, thres, metric_original)
 
 print('######Heuristic - Closest Distance - Success Metric######')
-minval, maxval, thres, metric_original = 0., -1.0, -1e-1, closeset_distance_metrics
+minval, maxval, thres, metric_original = 0., -1.0, dis_thres, closeset_distance_metrics
 get_s_metric(minval, maxval, thres, metric_original)
 
 print('######6. Heuristic - Contact Force - Maneuverability Metric######')
-minval, maxval, thres, metric_original = 0., 1.0, 1.0, contact_force_metrics
+minval, maxval, thres, metric_original = 0., 1.0, force_thres, contact_force_metrics
 get_m_metric(minval, maxval, thres, metric_original)
 
 print('######Heuristic - Contact Force - Success Metric######')
-minval, maxval, thres, metric_original = 0., 1.0, 1.0, contact_force_metrics
+minval, maxval, thres, metric_original = 0., 1.0, force_thres, contact_force_metrics
 get_s_metric(minval, maxval, thres, metric_original)
