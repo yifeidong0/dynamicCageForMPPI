64a53ed053b898019407f3b3f26d35290d56cb81
YvesDong <602522935@qq.com>
2024-01-23 19:46:55 +0100

    herding success metric evak

--- scripts/herding_postprocess.py
+++ scripts/herding_postprocess.py
@@ -5,31 +5,43 @@ import csv
 
 # Read from the CSV file
 best_costs = []
-file_num_herders = 'data/evaluation/herding/num_herders/effort-aorrt/ao_rrt.csv'
+success_metric = []
+file_num_herders = 'data/evaluation/herding/num_herders/prob-aoest/ao_est_1.csv'
 with open(file_num_herders, 'r') as file:
     csv_reader = csv.reader(file)
     header = next(csv_reader)
     for id, row in enumerate(csv_reader):
         best_costs.append(float(row[4]))
-print(len(best_costs)) # 140
+        success_metric.append(float(row[5]))
 
-num_robots_list = list(range(1, 15))
-num_samples = 10 # 10 samples per number of robots
+num_robots_list = list(range(4,9))
+num_samples = 6 # 6 samples per number of robots
 mean_costs = []
 std_costs = []
+mean_metrics = []
+std_metrics = []
 for num_robots in num_robots_list:
     costs = []
+    metrics = []
     for i in range(num_samples):
-        costs.append(best_costs[i + (num_robots-1)*num_samples])
+        costs.append(best_costs[i + (num_robots-num_robots_list[0])*num_samples])
+        metrics.append(success_metric[i + (num_robots-num_robots_list[0])*num_samples])
     mean_costs.append(np.mean(costs))
     std_costs.append(np.std(costs))
-print(mean_costs)
-print(std_costs)
+    mean_metrics.append(np.mean(metrics))
+    std_metrics.append(np.std(metrics))
 
-# Plot costs vs. number of robots in terms of mean and std
+# # Plot costs vs. number of robots in terms of mean and std
+# plt.figure()
+# plt.errorbar(num_robots_list, mean_costs, yerr=std_costs, fmt='o')
+# plt.xlabel('Number of robots')
+# plt.ylabel('Best cost')
+# # plt.title('AO-RRT')
+# plt.show()
+
+# Plot success metric vs. number of robots in terms of mean and std
 plt.figure()
-plt.errorbar(num_robots_list, mean_costs, yerr=std_costs, fmt='o')
+plt.errorbar(num_robots_list, mean_metrics, yerr=std_metrics, fmt='o')
 plt.xlabel('Number of robots')
-plt.ylabel('Best cost')
-# plt.title('AO-RRT')
+plt.ylabel('Success metric')
 plt.show()
